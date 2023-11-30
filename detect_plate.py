7e55c611f82650179e88e67457ab7f079c44ab34
Linorman <3033797357@qq.com>
2023-11-30 21:46:11 +0800

    simplify codes

--- detect_plate.py
+++ detect_plate.py
@@ -189,45 +189,17 @@ def detect_Recognition_plate(model, orgimg, device, plate_rec_model, img_size, i
     # cv2.imwrite('result.jpg', orgimg)
 
 
-def draw_result(orgimg, dict_list, is_color=False):  # 车牌结果画出来
+def print_result(dict_list, is_color=False):  # 车牌结果画出来
     result_str = ""
     for result in dict_list:
-        rect_area = result['rect']
-
-        x, y, w, h = rect_area[0], rect_area[1], rect_area[2] - rect_area[0], rect_area[3] - rect_area[1]
-        padding_w = 0.05 * w
-        padding_h = 0.11 * h
-        rect_area[0] = max(0, int(x - padding_w))
-        rect_area[1] = max(0, int(y - padding_h))
-        rect_area[2] = min(orgimg.shape[1], int(rect_area[2] + padding_w))
-        rect_area[3] = min(orgimg.shape[0], int(rect_area[3] + padding_h))
-
-        height_area = result['roi_height']
-        landmarks = result['landmarks']
         result_p = result['plate_no']
         if result['plate_type'] == 0:  # 单层
             result_p += " " + result['plate_color']
         else:  # 双层
             result_p += " " + result['plate_color'] + "双层"
         result_str += result_p + " "
-        for i in range(4):  # 关键点
-            cv2.circle(orgimg, (int(landmarks[i][0]), int(landmarks[i][1])), 5, clors[i], -1)
-        cv2.rectangle(orgimg, (rect_area[0], rect_area[1]), (rect_area[2], rect_area[3]), (0, 0, 255), 2)  # 画框
-
-        labelSize = cv2.getTextSize(result_p, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 1)  # 获得字体的大小
-        if rect_area[0] + labelSize[0][0] > orgimg.shape[1]:  # 防止显示的文字越界
-            rect_area[0] = int(orgimg.shape[1] - labelSize[0][0])
-        orgimg = cv2.rectangle(orgimg, (rect_area[0], int(rect_area[1] - round(1.6 * labelSize[0][1]))),
-                               (int(rect_area[0] + round(1.2 * labelSize[0][0])), rect_area[1] + labelSize[1]),
-                               (255, 255, 255), cv2.FILLED)  # 画文字框,背景白色
-
-        if len(result) >= 1:
-            orgimg = cv2ImgAddText(orgimg, result_p, rect_area[0], int(rect_area[1] - round(1.6 * labelSize[0][1])),
-                                   (0, 0, 0), 21)
-            # orgimg=cv2ImgAddText(orgimg,result_p,rect_area[0]-height_area,rect_area[1]-height_area-10,(0,255,0),height_area)
-
     print(result_str)
-    return orgimg
+    return result_str
 
 
 def get_second(capture):
@@ -285,14 +257,11 @@ if __name__ == '__main__':
                 # detect_one(model,img_path,device)
                 dict_list = detect_Recognition_plate(detect_model, img, device, plate_rec_model, opt.img_size,
                                                      is_color=opt.is_color)  # 检测以及识别车牌
-                ori_img = draw_result(img, dict_list)  # 将结果画在图上
-                img_name = os.path.basename(img_path)
-                save_img_path = os.path.join(save_path, img_name)  # 图片保存的路径
+                print_result(dict_list)  # 将结果画在图上
                 time_e = time.time()
                 time_gap = time_e - time_b  # 计算单个图片识别耗时
                 if count:
                     time_all += time_gap
-                cv2.imwrite(save_img_path, ori_img)  # opencv将识别的图片保存
                 count += 1
             print(
                 f"sumTime time is {time.time() - time_begin} s, average pic time is {time_all / (len(file_list) - 1)}")
@@ -304,11 +273,7 @@ if __name__ == '__main__':
             # detect_one(model,img_path,device)
             dict_list = detect_Recognition_plate(detect_model, img, device, plate_rec_model, opt.img_size,
                                                  is_color=opt.is_color)
-            ori_img = draw_result(img, dict_list)
-            img_name = os.path.basename(opt.image_path)
-            save_img_path = os.path.join(save_path, img_name)
-            cv2.imwrite(save_img_path, ori_img)
-
+            print_result(dict_list)
 
     else:  # 处理视频
         video_name = opt.video
@@ -332,30 +297,12 @@ if __name__ == '__main__':
                 img0 = copy.deepcopy(img)
                 dict_list = detect_Recognition_plate(detect_model, img, device, plate_rec_model, opt.img_size,
                                                      is_color=opt.is_color)
-                ori_img = draw_result(img, dict_list)
+                print_result(dict_list)
                 t2 = cv2.getTickCount()
                 infer_time = (t2 - t1) / cv2.getTickFrequency()
                 fps = 1.0 / infer_time
                 fps_all += fps
                 str_fps = f'fps:{fps:.4f}'
-
-                cv2.putText(ori_img, str_fps, (20, 20), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-                # cv2.imshow("haha",ori_img)
-                # cv2.waitKey(1)
-                out.write(ori_img)
-
-                # current_time = int(frame_count/FrameNumber*duration)
-                # sec = current_time%60
-                # minute = current_time//60
-                # for result_ in result_list:
-                #     plate_no = result_['plate_no']
-                #     if not is_car_number(pattern_str,plate_no):
-                #         continue
-                #     print(f'车牌号:{plate_no},时间:{minute}分{sec}秒')
-                #     time_str =f'{minute}分{sec}秒'
-                #     writer.writerow({"车牌":plate_no,"时间":time_str})
-                # out.write(ori_img)
-
 
         else:
             print("失败")
